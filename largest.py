9cdbedfdbb5c63154816a3f461e39bd18c9dd691
luvega-rj <luvegaronny@gmail.com>
2023-05-15 15:30:43 +0300

    completed assignment

---
+++ largest.py
@@ -0,0 +1,29 @@
+my_list = [19,13,12,10,92,390,999]
+my_list.sort(reverse=True)
+print(my_list[0])
+arr = [25, 11, 7, 75, 56];
+max = arr[0];
+for i in range(0, len(arr)):
+  if(arr[i] < max):
+       max = arr[i];
+print( int(str(max)));
+# arr = [25, 11, 7, 75, 56];
+# min = arr[0];
+
+# for i in range(0, len(arr)):
+#   if(arr[i] < min):
+#        min = arr[i];
+# print( int(str(min)));
+# arr = [25, 11, 7, 75, 56]
+# largest = max(arr)
+# arr.remove(largest)
+# second_largest = max(arr)
+# print(second_largest)
+
+# arr = [25, 11, 7, 75, 56]
+# def number(arr):
+#     arr.sort()
+#     print(arr[0])
+#     print(arr[-1])
+#     print(arr[-2])
+# number(arr)
